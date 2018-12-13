d245b15b0eca1256b61bc162702388627a12bcec
Travis Bumgarner <travis-bumgarner@pluralsight.com>
2018-12-12 21:55:30 -0500

    (slack_express) Hooked up to sqs and added message validation

--- slack_express/src/app.js
+++ slack_express/src/app.js
@@ -7,14 +7,15 @@ import * as routes from "./routes"
 import config from "./config"
 import qs from 'querystring'
 
+const ALLOWABLE_MESSAGE_LENGTH = 20
+
 AWS.config.update({
   region: config.sqs.region,
-  accessKeyId: config.sqs.access_key_id,
-  secretAccessKey: config.sqs.secret_access_key
+  accessKeyId: config.sqs.accessKeyId,
+  secretAccessKey: config.sqs.secretAccessKey
 });
 
 const SLACK_URL = 'https://slack.com/api';
-
 
 const app = express()
 const sqs = new AWS.SQS()
@@ -25,12 +26,12 @@ app.use(bodyParser.json())
 
 app.use(middleware.validateSlackRequest)
 
-app.post("/message", async (request, response, next) => {
-    const responseBody = await routes.message(request.body)
-    response.send(responseBody)
-})
+// app.post("/message", async (request, response, next) => {
+//     const responseBody = await routes.message(request.body)
+//     response.send(responseBody)
+// })
 
-app.post("/message2", async (request, response, next) => {
+app.post("/dialog", async (request, response, next) => {
     const { trigger_id } = request.body
 
     const dialog = {
@@ -51,7 +52,7 @@ app.post("/message2", async (request, response, next) => {
               {
                 "type": "text",
                 "label": "Feeling stylish?",
-                "name": "color"
+                "name": "colors"
             }
           ]
         })
@@ -66,12 +67,44 @@ app.post("/message2", async (request, response, next) => {
       });
 })
 
-// app.get("/auth", async (request, response, next) => {
-//     const responseBody = await routes.auth(request)
-//     response.redirect("http://letspair.online/welcome")
-// })
+app.post("/form_submission", async (request, response, next) => {
+    const data = JSON.parse(request.body.payload)
+    const {submission:{message, colors}, user:{id, name} } = data 
+    console.log(message,colors, id, name)
+    if(message.length > ALLOWABLE_MESSAGE_LENGTH){
+        response.json({
+            "errors": [
+              {
+                "name": "message",
+                "error": "The Demogorgon ate you because your message was too long. Please pick a shorter message. (<20)"
+              },
+            ]
+          })
+    }
+
+    // const responseBody = await routes.message({message, colors, id, name})  
+    // Slack API doesn't seem to care about messages in the next line
+    const result = await queue_sqs(message)
+    console.log('result', result)
+    response.send('')
+})
 
 app.get("/ok", (request, response, next) => response.send("Service is running"))
+
+const queue_sqs = (message) => {
+    const params = {
+        MessageBody: message,
+        QueueUrl: config.sqs.url
+    }
+    sqs.sendMessage(params, (error, data) => { 
+        if (error) {
+            console.log(error.stack)
+            return false
+        } 
+        console.log(data)
+        return true
+    }) 
+}
 
 app.get("/test-sqs", async (request, response, next) => {
     const params = {
